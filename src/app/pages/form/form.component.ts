97e1f4f650f12e9defd7793cf0d43e2f5dde3170
Atul Kesharwani <atul.kesharwani@precisebh.com>
2025-03-15 14:13:45 +0530

    Common Module and Forms Module

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { User } from '../../model/model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+
+  user: User = { name: '', email: '', age: null }; // ✅ Use interface
+
+  submitForm() {
+    console.log('Form Data:', this.user); // ✅ Log form data
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,13 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { FormComponent } from '../form/form.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [FormComponent,NavbarComponent],
+  imports: [FormComponent,NavbarComponent,CommonModule,FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -13,6 +16,7 @@ export class HomeComponent implements OnInit {
   constructor(private apiService: ApiService) { } // ✅ Injecting the service
   
   title:String='I love You Bua';
+  order:any[]=['Atul','Arpit','Akash','Duggu'];
 
   updateTitle(newTitle:string):void{
     this.title=newTitle;
